22f3563bd34eec6dbac38be24325855776a9fbc3
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-05-08 19:02:31 +0200

    Added UnitTest for StateMachine

---
+++ UnitTest_StateMachine.cpp
@@ -0,0 +1,23 @@
+//
+// UnitTest_StateMachine.cpp for  in /home/nicolaschr/Work/cpp_bomberman
+//
+// Made by Nicolas Charvoz
+// Login   <nicolaschr@epitech.net>
+//
+// Started on  Fri May  8 13:00:22 2015 Nicolas Charvoz
+// Last update Fri May  8 13:14:19 2015 Nicolas Charvoz
+//
+
+#include "StateMachine.hh"
+
+int		main(int ac, char **av)
+{
+  (void)ac;
+  (void)av;
+
+  StateMachine sM;
+  void *uT;
+
+  sM.ActivateAction(uT);
+  return (0);
+}
