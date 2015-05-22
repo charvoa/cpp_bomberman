7a8a6509bc5c216c2820a93a96906566d2c03a05
antgar <antoine.garcia@epitech.eu>
2015-05-22 11:26:21 +0200

    Update World.hh

--- World.hh
+++ World.hh
@@ -5,7 +5,7 @@
 // Login   <antgar@epitech.net>
 //
 // Started on  Fri May 22 10:10:43 2015 Antoine Garcia
-// Last update Fri May 22 10:21:49 2015 Antoine Garcia
+// Last update Fri May 22 10:52:43 2015 Antoine Garcia
 //
 
 #ifndef WORLD_HH__
@@ -14,15 +14,17 @@
 #include <list>
 #include "GameState.hh"
 #include "ACharacter.hh"
+#include "HumanCharacter.hh"
 
 class	World : public GameState
 {
 private:
-  std::list<ACharacter *> _players;
+  std::list<ACharacter *> _ia;
   int			_nbPlayers;
   int			_nbIa;
   Map			_map;
-
+  HumanCharacter	*player1;
+  HumanCharacter	*_player2;
 public:
   World(Map &map, int nb_players, int nb_ia);
 };
