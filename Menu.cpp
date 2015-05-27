bdf976e7592e3df745ac7d535b76ecfb68900c57
antgar <antoine.garcia@epitech.eu>
2015-05-27 15:39:37 +0200

    Add Pause sound

--- Menu.cpp
+++ Menu.cpp
@@ -5,7 +5,7 @@
 // Login   <antgar@epitech.net>
 //
 // Started on  Fri May  8 13:43:01 2015 Antoine Garcia
-// Last update Wed May 27 13:20:43 2015 Nicolas Charvoz
+// Last update Wed May 27 15:38:09 2015 Antoine Garcia
 //
 
 #include "Menu.hh"
@@ -18,7 +18,7 @@ Menu::Menu(Game *game)
   _game = game;
   _sound.registerSound("./resources/sounds/menu.wav", "main");
   _sound.registerSound("./resources/sounds/beretta.mp3", "shot");
-  //_sound.playMusic("main");
+  _sound.playMusic("main", 1);
 
   _texManag.registerTexture("background-desert", "backgroundMenu");
   _texManag.registerTexture("ExitButton", "exit");
