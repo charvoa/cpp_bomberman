7593b74a27ef74210958feaf3013dec50c92b289
audibe_l <audibe_l@epitech.eu>
2015-05-28 17:13:42 +0200

    move

--- HumanCharacter.cpp
+++ HumanCharacter.cpp
@@ -5,7 +5,7 @@
 // Login   <nicolaschr@epitech.net>
 //
 // Started on  Tue May 19 11:55:01 2015 Nicolas Charvoz
-// Last update Thu May 28 16:54:08 2015 Audibert Louis
+// Last update Thu May 28 17:13:11 2015 Audibert Louis
 //
 
 #include "HumanCharacter.hh"
@@ -164,5 +164,10 @@ void	HumanCharacter::move(e_orientation ori)
     pos = new Position(_pos._x, _pos._y - 1);
   else if (ori == LEFT)
     pos = new Position(_pos._x - 1, _pos._y);
+  _pos = *pos;
+  
+  glm::vec3 move(0 + (_pos._x - _world->getWidth() / 2) * 100, 0,  750 * (-1) + (_pos._y - _world->getHeight() / 2) * 100);
+  this->translate(move);
+
   _world->setItemAtPosition(*pos, _id);
 }
