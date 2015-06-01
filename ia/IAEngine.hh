f16b62afde3338b41078ec352310d39531e632c3
Serge Heitzler <serge.heitzler@epitech.eu>
2015-06-01 16:34:45 +0200

    ia 70%

--- ia/IAEngine.hh
+++ ia/IAEngine.hh
@@ -1,14 +1,36 @@
 #ifndef IAEngine_HH_
 # define IAEngine_HH_
 
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include "ACharacter.hh"
+
 class		IAEngine
 {
 public:
   IAEngine();
   ~IAEngine();
+  int			calculLength(int firstPosX, int secondPosX);
+  int			calculWidth(int firstPosY, int secondPosY);
+  float			calculDistance(IACharacter *ia, HumanCharacter *human);
+  ACharacter	*findClosestHumanPlayer(IACharacter *ia, World *world);
+  ACharacter	*getTarget(IACharacter *ia, World *world);
+  bool      	isBombAroundMe(std::vector<char> &vector);
+  bool      	isBoxAroundMe(std::vector<char> &vector);
+  bool      	isHumanPlayerAroundMe(std::vector<char> &vector);
 
 private:
-  
+  int           _w;
+  int           _l;
+  int           _xIA;
+  int           _yIA;
+  int           _xHuman;
+  int           _yHuman;
+  float   		_h1;
+  float    		_h2;
+  Math			_math;
+  std::vector<char> _aroundMe;
 
 };
 
